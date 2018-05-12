dd9d52a9e865d77b09bcc49307e8729bde0a25ed
serbulas1 <serbulas1@gmail.com>
2018-05-12 16:04:51 +0200

    commit pre enuma

---
+++ AMI_Device/AMICharacteristics.cs
@@ -0,0 +1,25 @@
+﻿using MlkPwgen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMI_Device
+{
+    public class AMICharacteristics
+    {
+        public string Name { get; set; }
+
+        public int CreationTime { get; set; }
+
+        public Dictionary<Enum, double> Measurements { get; set; }
+
+        public AMICharacteristics()
+        {
+            Measurements = new Dictionary<Enum, double>();
+            Name = PasswordGenerator.Generate(length: 10, allowed: Sets.Alphanumerics); //generise random string
+            CreationTime = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds; // vreme po unix timestamp formatu (racuna od 1970 do danas sekunde)
+        }
+    }
+}
