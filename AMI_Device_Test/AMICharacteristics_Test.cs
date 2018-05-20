db6cbf041496caa2f5742fdb8a3397a5398e627d
serbulas1 <serbulas1@gmail.com>
2018-05-20 20:25:24 +0200

    odradjen test s dictionary - ne d'o bog nikome

--- AMI_Device_Test/AMICharacteristics_Test.cs
+++ AMI_Device_Test/AMICharacteristics_Test.cs
@@ -45,5 +45,35 @@ namespace AMI_Device_Test
             //Assert.IsNotEmpty(ami.Device_code);
             CollectionAssert.IsEmpty(ami.Measurements);
         }
+
+        static object[] MySourceProperty
+        {
+            get
+            {
+                var measurement = new Dictionary<TypeMeasurement, double>()
+                {
+                    {TypeMeasurement.ActivePower,120 },
+                    {TypeMeasurement.CurrentP,220.23 },
+                    {TypeMeasurement.ReactivePower,69 },
+                    {TypeMeasurement.Voltage,10.4 }
+                };
+                // Do what you want with your dictionary
+
+                // The order of element in the object must be the same expected by your test method
+                //yield return new object[] { "agr1", "askdwskdls", measurement }; ne moze yield iz nekog razloga
+                return new[] { new object[] { "agr1", "askdwskdls", measurement } };
+
+            }
+        }
+
+
+        [Test]
+        [TestCaseSource("MySourceProperty")]
+        public void SendDataToAgregator_GoodVariables_ReturnsOn(string agrID,string devID, Dictionary<TypeMeasurement, double> measurement)
+        {
+            Assert.IsNotEmpty(agrID);
+            Assert.IsNotEmpty(devID);
+            CollectionAssert.IsNotEmpty(measurement);
+        }
     }
 }
