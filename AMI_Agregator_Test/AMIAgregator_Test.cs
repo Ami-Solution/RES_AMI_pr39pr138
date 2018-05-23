44ce3bc8c91af7e8990dec305326524c4937e84e
serbulas1 <serbulas1@gmail.com>
2018-05-23 05:24:39 +0200

    AMIChar gotov

---
+++ AMI_Agregator_Test/AMIAgregator_Test.cs
@@ -0,0 +1,24 @@
+﻿using AMI_Agregator;
+using NUnit.Framework;
+using System;
+
+
+namespace AMI_Agregator_Test
+{
+    [TestFixture]
+    public class AMIAgregator_Test
+    {
+        [Test]
+        [TestCase("agregator1")]
+        [TestCase("agregator12")]
+        [TestCase("agregator123")]
+        [TestCase("agregator0")]
+        public void AMIAgregator_GoodParametersInConstructor_Instantiated(string name)
+        {
+            AMIAgregator agr = new AMIAgregator(name);
+            MainWindow.agregatorNumber++;
+
+            Assert.AreEqual(agr.Agregator_code, name);
+        }
+    }
+}
---
+++ AMI_Agregator_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AMI_Agregator_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AMI_Agregator_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fada3321-51cb-4f85-bb85-709b290ad9ce")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
